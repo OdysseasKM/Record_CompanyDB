3166e7b8f5562b5662357a70c1edb752058e59f1
Odysseas Kopakakis <od.kopakakis@gmail.com>
2022-12-09 13:32:06 +0200

    2nd edition

--- main.py
+++ main.py
@@ -6,6 +6,21 @@ def menu():
     choice = int(input())
     return choice
 
+def pick(options):
+    print("Please choose:")
+
+    for idx, element in enumerate(options):
+        print("{}) {}".format(idx + 1, element))
+
+    i = input("Enter number: ")
+    try:
+        if 0 < int(i) <= len(options):
+            return int(i) - 1
+    except:
+        pass
+    return None
+
+
 sql = ""
 db = sqlite3.connect('record-company.db')
 cursor = db.cursor()
@@ -20,12 +35,50 @@ if choice == 1:
     cursor.execute(sql,(idn, nickname))
 
 if choice == 2:
-    artist_id = int(input("Give Artist ID: "))
+    artist_name = input("Give Artist Name: ")
     idn = int(input("Give ID: "))
     name = input("Give the name of the release: ")
+
+    options = ["Album", "Video", "Single"]
+    category = pick(options)
+
+    options2 = ["Album", "Video", "Single"]
+    format = pick(options2)
+    
+
+    sql = """SELECT id
+    FROM ARTIST
+    WHERE ARTIST.nickname= ?;"""
+    artist_id= cursor.execute(sql, (artist_name,)).fetchone()[0]
+
     sql = """INSERT INTO RELEASE
     VALUES (?, ?, '8/12/2022', ?);"""
     cursor.execute(sql, (artist_id, idn, name))
+
+    if category == 0:
+        sql = """INSERT INTO ALBUM
+        VALUES (?,?);"""
+        cursor.execute(sql, (idn, name))
+
+    elif category == 1:
+        duration = int(input("Give duration: "))
+
+        sql = """INSERT INTO VIDEO
+        VALUES (?,?);"""
+        cursor.execute(sql, (idn, duration))
+
+    elif category == 2:
+        duration = int(input("Give duration: "))
+        writer_ssn = int(input("Give writer's Ssn: "))
+
+        sql = """INSERT INTO SONG
+        VALUES (?, ?, ?, ?);"""
+        cursor.execute(sql, (idn, None, duration, None))
+        sql = """INSERT INTO WRITER
+        VALUES (?, 'John', 'Efthimiou');"""
+        cursor.execute(sql, (writer_ssn,))
+
+
 
 if choice == 3:
     sql = """SELECT *
