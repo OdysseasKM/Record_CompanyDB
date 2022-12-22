2680c837de1d323446b99bd6019ab6f32f108727
John Efthymiou <johnefthimiou1002@gmail.com>
2022-12-23 00:40:57 +0200

    sql update

--- add_data_in_db.sql
+++ add_data_in_db.sql
@@ -1,20 +1,25 @@
-insert into ARTIST (id,nickname,country) values (1, "john", "greece");
-insert into ARTIST (id,nickname,country) values (2, "ody", "greece");
-insert into ARTIST (id,nickname,country) values (3, "john the great", "greece");
-insert into ARTIST (id,nickname,country) values (4, "demitria", "albania");
+insert into ARTIST (id,nickname,origin) values (1, "john", "greece");
+insert into ARTIST (id,nickname,origin) values (2, "ody", "greece");
+insert into ARTIST (id,nickname,origin) values (3, "john the great", "greece");
+insert into ARTIST (id,nickname,origin) values (4, "demitria", "albania");
 
-insert into RELEASE (artist_id, release_id, r_date, release_title) values (1, 1, "10/10/2000", "new release");
-insert into RELEASE (artist_id, release_id, r_date, release_title) values (1, 2, "10/10/2000", "new release1");
-insert into RELEASE (artist_id, release_id, r_date, release_title) values (1, 3, "10/10/2000", "new release2");
-insert into RELEASE (artist_id, release_id, r_date, release_title) values (2, 4, "10/10/2000", "new release3");
-insert into RELEASE (artist_id, release_id, r_date, release_title) values (2, 5, "10/10/2000", "new release4");
-insert into RELEASE (artist_id, release_id, r_date, release_title) values (2, 6, "10/10/2000", "new release5");
-insert into RELEASE (artist_id, release_id, r_date, release_title) values (3, 7, "10/10/2000", "new release6");
-insert into RELEASE (artist_id, release_id, r_date, release_title) values (3, 8, "10/10/2000", "new release7");
-insert into RELEASE (artist_id, release_id, r_date, release_title) values (3, 9, "10/10/2000", "new release8");
-insert into RELEASE (artist_id, release_id, r_date, release_title) values (3, 10, "10/10/2000", "new release8");
-insert into RELEASE (artist_id, release_id, r_date, release_title) values (5, 11, "10/10/2000", "kapsoura 8 mwromou");
-insert into RELEASE (artist_id, release_id, r_date, release_title) values (5, 12, "10/10/2000", "stepbrother i am stuck / hot mommy milker mom");
+INSERT into GENRE(g_name, g_id) values ('pop',1);
+INSERT into GENRE(g_name, g_id) values ('indie, rock',2);
+INSERT into GENRE(g_name, g_id) values ('indie',3);
+INSERT into GENRE(g_name, g_id) values ('punk',4);
+
+insert into RELEASE (artist_id, rel_id, r_date, release_title, genre_id) values (1, 1, "10/10/2000", "new release", 1);
+insert into RELEASE (artist_id, rel_id, r_date, release_title, genre_id) values (1, 2, "10/10/2000", "new release1", 1);
+insert into RELEASE (artist_id, rel_id, r_date, release_title, genre_id) values (1, 3, "10/10/2000", "new release2", 1);
+insert into RELEASE (artist_id, rel_id, r_date, release_title, genre_id) values (2, 4, "10/10/2000", "new release3", 4);
+insert into RELEASE (artist_id, rel_id, r_date, release_title, genre_id) values (2, 5, "10/10/2000", "new release4", 2);
+insert into RELEASE (artist_id, rel_id, r_date, release_title, genre_id) values (2, 6, "10/10/2000", "new release5", 1);
+insert into RELEASE (artist_id, rel_id, r_date, release_title, genre_id) values (3, 7, "10/10/2000", "new release6", 3);
+insert into RELEASE (artist_id, rel_id, r_date, release_title, genre_id) values (3, 8, "10/10/2000", "new release7", 1);
+insert into RELEASE (artist_id, rel_id, r_date, release_title, genre_id) values (3, 9, "10/10/2000", "new release8", 2);
+insert into RELEASE (artist_id, rel_id, r_date, release_title, genre_id) values (3, 10, "10/10/2000", "new release8", 1);
+insert into RELEASE (artist_id, rel_id, r_date, release_title, genre_id) values (4, 11, "10/10/2000", "kapsoura 8 mwromou", 2);
+insert into RELEASE (artist_id, rel_id, r_date, release_title, genre_id) values (4, 12, "10/10/2000", "stepbrother i am stuck / hot mommy milker mom", 3);
 
 insert into SONG (song_id) values (6);
 insert into SONG (song_id) values (7);
@@ -32,40 +37,28 @@ insert into VIDEO (video_id, duration) values (5, 74);
 insert into VIDEO (video_id, duration) values (12, 500);
 
 
-INSERT into GENRE(name, rel_id) values ('pop',1);
-INSERT into GENRE(name, rel_id) values ('pop',2);
-INSERT into GENRE(name, rel_id) values ('indie',3);
-INSERT into GENRE(name, rel_id) values ('pop',4);
-INSERT into GENRE(name, rel_id) values ('pop',5);
-INSERT into GENRE(name, rel_id) values ('indie',6);
-INSERT into GENRE(name, rel_id) values ('pop',7);
-INSERT into GENRE(name, rel_id) values ('pop',8);
-INSERT into GENRE(name, rel_id) values ('indie',9);
-INSERT into GENRE(name, rel_id) values ('laikh-indie',11);
-INSERT into GENRE(name, rel_id) values ('punk',12);
-INSERT into GENRE(name, rel_id) values ('trap',12);
 
-insert into FORMAT (format_id, rel_id) values (1,1);
-insert into FORMAT (format_id, rel_id) values (2,2);
-insert into FORMAT (format_id, rel_id) values (3,3);
-insert into FORMAT (format_id, rel_id) values (4,4);
-insert into FORMAT (format_id, rel_id) values (5,5);
-insert into FORMAT (format_id, rel_id) values (6,6);
-insert into FORMAT (format_id, rel_id) values (7,7);
-insert into FORMAT (format_id, rel_id) values (8,8);
-insert into FORMAT (format_id, rel_id) values (9,9);
-insert into FORMAT (format_id, rel_id) values (10,11);
-insert into FORMAT (format_id, rel_id) values (11,12);
+insert into FORMAT (rel_id, format_id) values (1,1);
+insert into FORMAT (rel_id, format_id) values (2,2);
+insert into FORMAT (rel_id, format_id) values (3,3);
+insert into FORMAT (rel_id, format_id) values (4,4);
+insert into FORMAT (rel_id, format_id) values (5,5);
+insert into FORMAT (rel_id, format_id) values (6,6);
+insert into FORMAT (rel_id, format_id) values (7,7);
+insert into FORMAT (rel_id, format_id) values (8,8);
+insert into FORMAT (rel_id, format_id) values (9,9);
+insert into FORMAT (rel_id, format_id) values (10,11);
+insert into FORMAT (rel_id, format_id) values (11,12);
 
 
 
-insert into ONLINE (id, views) values (1,1000);
-insert into ONLINE (id, views) values (2,1001);
-insert into ONLINE (id, views) values (3,1002);
-insert into ONLINE (id, views) values (4,1003);
-insert into ONLINE (id, views) values (5,1004);
-insert into ONLINE (id, views) values (9,7);
-insert into ONLINE (id, views) values (11, 7000000);
+insert into DIGITAL (format_id, views) values (1,1000);
+insert into DIGITAL (format_id, views) values (2,1001);
+insert into DIGITAL (format_id, views) values (3,1002);
+insert into DIGITAL (format_id, views) values (4,1003);
+insert into DIGITAL (format_id, views) values (5,1004);
+insert into DIGITAL (format_id, views) values (9,7);
+insert into DIGITAL (format_id, views) values (11, 7000000);
 
 
 
--- record_company_sqlite_create.sql
+++ record_company_sqlite_create.sql
@@ -1,186 +1,175 @@
-DROP TABLE IF EXISTS ARTIST;
-CREATE TABLE IF NOT EXISTS ARTIST
-(
+CREATE TABLE ARTIST (
 	id integer NOT NULL,
 	nickname varchar NOT NULL,
-	country varchar,
+	origin varchar,
 	PRIMARY KEY (id)
-);
-
-DROP TABLE IF EXISTS RELEASE;
-CREATE TABLE IF NOT EXISTS RELEASE 
-(
-	artist_id integer NOT NULL,
-	release_id integer NOT NULL,
-	r_date date,
-	release_title varchar,
-	PRIMARY KEY (release_id)
-);
-
-DROP TABLE IF EXISTS FEATURE_IN;
-CREATE TABLE IF NOT EXISTS FEATURE_IN 
-(
-	artist_id integer NOT NULL,
-	rel_id integer NOT NULL,
-	FOREIGN KEY (artist_id)
-	REFERENCES ARTIST(id)
-    ON DELETE CASCADE,
-	FOREIGN KEY (rel_id)
-	REFERENCES RELEASE(release_id)
-    ON DELETE CASCADE
-);
-
-DROP TABLE IF EXISTS PUBLISH;
-CREATE TABLE IF NOT EXISTS PUBLISH 
-(
-	artist_id integer NOT NULL,
-	rel_id integer NOT NULL,
-	FOREIGN KEY (artist_id)
-	REFERENCES ARTIST(id)
-    ON DELETE CASCADE,
-	FOREIGN KEY (rel_id)
-	REFERENCES RELEASE(release_id)
-    ON DELETE CASCADE
-);
-
-DROP TABLE IF EXISTS ALBUM;
-CREATE TABLE IF NOT EXISTS ALBUM 
-(
-	id integer NOT NULL,
-	a_name varchar,
-	PRIMARY KEY (id)
-);
-
-DROP TABLE IF EXISTS SONG;
-CREATE TABLE IF NOT EXISTS SONG
-(
-    song_id integer NOT NULL,
-    album_id integer ,
-    writer_ssn varchar,
-    duration time,
-    video_id integer ,
-    studio_id integer,
-    PRIMARY KEY (song_id),
-    FOREIGN KEY (video_id)
-    REFERENCES VIDEO(video_id)
-    ON DELETE CASCADE,
-    FOREIGN KEY (album_id)
-    REFERENCES ALBUM(id)
-    ON DELETE CASCADE,
-    FOREIGN KEY (song_id)
-    REFERENCES RELEASE(release_id)
-    ON DELETE CASCADE,
-    FOREIGN KEY (writer_Ssn)
-    REFERENCES WRITER(Ssn)
-    ON DELETE CASCADE
-    FOREIGN KEY (studio_id)
-    REFERENCES STUDIO(id)
-    ON DELETE CASCADE
-);
-
-DROP TABLE IF EXISTS VIDEO;
-CREATE TABLE IF NOT EXISTS VIDEO 
-(
-	video_id integer NOT NULL,
-	duration time,
-	PRIMARY KEY (video_id)
-);
-
-
-DROP TABLE IF EXISTS WRITER;
-CREATE TABLE IF NOT EXISTS WRITER 
-(
-	Ssn varchar,
-	first_name varchar,
-	last_name varchar,
-	PRIMARY KEY (Ssn)
-);
-
-DROP TABLE IF EXISTS STUDIO;
-CREATE TABLE IF NOT EXISTS STUDIO
-(
-    id integer NOT NULL,
-    street varchar,
-    number varchar,
-    town varchar,
-    country varchar,
-    PRIMARY KEY (id)
-);
-
-
-DROP TABLE IF EXISTS LYRICS;
-CREATE TABLE IF NOT EXISTS LYRICS
-(
-	song_id integer NOT NULL,
-	language varchar,
-	l_text text,
-	FOREIGN KEY (song_id)
-	REFERENCES SONG(song_id)
-    ON DELETE CASCADE
-);
-
-DROP TABLE IF EXISTS RATING;
-CREATE TABLE IF NOT EXISTS RATING 
-(
-	stars integer ,
-	rel_id integer NOT NULL,
-	FOREIGN KEY (rel_id)
-	REFERENCES RELEASE(release_id)
-    ON DELETE CASCADE
 );
 
 DROP TABLE IF EXISTS GENRE;
-CREATE TABLE IF NOT EXISTS GENRE 
-(
-	name varchar,
+CREATE TABLE GENRE (
+	g_name varchar NOT NULL,
+	g_id integer NOT NULL,
+	PRIMARY KEY (g_id)
+);
+
+
+CREATE TABLE RELEASE (
 	rel_id integer NOT NULL,
+	artist_id integer NOT NULL,
+	r_date date,
+	release_title varchar NOT NULL,
+	genre_id integer,
+	PRIMARY KEY (rel_id),
+
+	FOREIGN KEY (artist_id)
+	REFERENCES ARTIST(id)
+    ON DELETE CASCADE
+
+	FOREIGN KEY (genre_id)
+	REFERENCES GENRE(g_id)
+);
+
+CREATE TABLE FEATURE_IN (
+	artist_id integer NOT NULL,
+	rel_id integer  NOT NULL,
+
+	FOREIGN KEY (artist_id)
+	REFERENCES ARTIST(id)
+    ON DELETE CASCADE,
+
 	FOREIGN KEY (rel_id)
-	REFERENCES RELEASE(release_id)
+	REFERENCES RELEASE(rel_id)
     ON DELETE CASCADE
 );
 
-DROP TABLE IF EXISTS FORMAT;
-CREATE TABLE IF NOT EXISTS FORMAT
-(	
+CREATE TABLE ALBUM (
+	album_id integer NOT NULL,
+	a_name varchar NOT NULL,
+	PRIMARY KEY (album_id),
+	FOREIGN KEY (album_id)
+	REFERENCES RELEASE(rel_id)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE VIDEO (
+	video_id integer NOT NULL,
+	duration time,
+	PRIMARY KEY (video_id)
+	FOREIGN KEY (video_id)
+    REFERENCES RELEASE(rel_id)
+    ON DELETE CASCADE
+);
+
+
+CREATE TABLE SONG (
+	song_id integer NOT NULL,
+	album_id integer,
+	duration time,
+	video_id time,
+	studio_id integer,
+	lyrics_language varchar,
+	PRIMARY KEY (song_id),
+
+	FOREIGN KEY (song_id)
+    REFERENCES RELEASE(rel_id)
+    ON DELETE CASCADE,
+
+	FOREIGN KEY (album_id)
+	REFERENCES ALBUM(album_id)
+    ON DELETE CASCADE,
+
+	FOREIGN KEY (video_id)
+    REFERENCES VIDEO(video_id)
+);
+
+
+CREATE TABLE INDIVIDUAL (
+	ssn varchar NOT NULL,
+	artist_id integer,
+	first_name varchar,
+	last_name varchar,
+	PRIMARY KEY (ssn),
+	FOREIGN KEY (artist_id)
+    REFERENCES ARTIST(artist_id)
+);
+
+CREATE TABLE STUDIO (
+	studio_id integer NOT NULL,
+	street varchar,
+	number varchar,
+	town varchar,
+	country varchar,
+	PRIMARY KEY (studio_id)
+);
+
+CREATE TABLE RATING (
+	stars integer NOT NULL,
+	rel_id integer NOT NULL,
+	FOREIGN KEY (rel_id)
+	REFERENCES RELEASE(rel_id)
+    ON DELETE CASCADE
+);
+
+
+CREATE TABLE CONTRIBUTOR (
+	last_name varchar,
+	first_name varchar,
+	ssn varchar NOT NULL,
+	PRIMARY KEY (ssn)
+);
+
+CREATE TABLE CONTIBUTS_IN (
+	contributor_id integer NOT NULL,
+	rel_id integer NOT NULL,
+	role varchar NOT NULL,
+
+	FOREIGN KEY (rel_id)
+	REFERENCES RELEASE(rel_id)
+    ON DELETE CASCADE,
+
+	FOREIGN KEY (contributor_id)
+	REFERENCES CONTRIBUTOR(ssn)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE FORMAT (
+	rel_id integer NOT NULL,
 	format_id integer NOT NULL,
-	rel_id integer NOT NULL,
-	PRIMARY KEY(format_id)
+	PRIMARY KEY (format_id),
+
 	FOREIGN KEY (rel_id)
-	REFERENCES RELEASE(release_id)
+	REFERENCES RELEASE(rel_id)
     ON DELETE CASCADE
 );
 
-DROP TABLE IF EXISTS VINYL;
-CREATE TABLE IF NOT EXISTS VINYL 
-(
-	id integer NOT NULL,
+CREATE TABLE VYNIL (
+	format_id integer NOT NULL, 
 	sales integer,
-	cost integer,
-	FOREIGN KEY (id)
+	cost float,
+	FOREIGN KEY (format_id)
 	REFERENCES FORMAT(format_id)
-    ON DELETE CASCADE
+	ON DELETE CASCADE
 );
 
-DROP TABLE IF EXISTS CD;
-CREATE TABLE IF NOT EXISTS CD
-(
-	id integer NOT NULL,
+CREATE TABLE CD (
+	format_id integer NOT NULL,
 	sales integer,
-	cost integer,
-	FOREIGN KEY (id)
+	cost float,
+	FOREIGN KEY (format_id)
 	REFERENCES FORMAT(format_id)
-    ON DELETE CASCADE
+	ON DELETE CASCADE
 );
 
-DROP TABLE IF EXISTS ONLINE;
-CREATE TABLE IF NOT EXISTS ONLINE 
-(
-	id integer NOT NULL,
+CREATE TABLE DIGITAL (
+	format_id integer NOT NULL,
 	views integer,
-	FOREIGN KEY (id)
+	FOREIGN KEY (format_id)
 	REFERENCES FORMAT(format_id)
-    ON DELETE CASCADE
+	ON DELETE CASCADE
 );
+
+
+
 
 
 
